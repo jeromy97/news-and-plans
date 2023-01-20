25fd822cb0381a95a0cf3cbba4024d6bc045733a
Jeromy Hettinga <jhettinga@fletcher.nl>
2023-01-20 15:30:03 +0100

    Add migrations

---
+++ database/migrations/2023_01_20_134548_create_plans_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('plans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->text('description')->default('');
+            $table->enum('special', ['0', '1'])->default('0');
+            $table->integer('position');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('plans');
+    }
+};
