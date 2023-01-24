af34a9f916a84426686c789665b4309141187805
Jeromy Hettinga <jhettinga@fletcher.nl>
2023-01-24 16:28:13 +0100

    Install Bootstrap 5

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [NewsController::class, 'list']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
